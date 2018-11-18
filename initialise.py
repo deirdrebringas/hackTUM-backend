0fcfbaa8c285f6cf612d220e19b63b157d2703fc
deirdreams <deirdrebringas@gmail.com>
2018-11-18 01:00:33 +0100

    change db name

--- initialise.py
+++ initialise.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from rhymer import Rhymer
 
 def main():
-    DB_NAME = "test_db"
+    DB_NAME = "hiphop"
     COLLECTION_NAME = "lyrics_collection"
     client = MongoClient()
     collection = client.get_database(DB_NAME).get_collection(COLLECTION_NAME)
